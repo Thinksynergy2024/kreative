0e60e07489631d3186c2375447d7acf6ad3819c5
Marcos8060 <marcosgav80@gmail.com>
2024-09-26 11:50:48 +0300

    polished the graphs

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,42 @@
 import AuthGuard from "@/assets/hoc/authguard";
 import CustomizedLayout from "@/components/layout/customized-layout";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ActiveTestGraph from "./graphs/active-test-graph";
 import CompleteTestGraph from "./graphs/complete-test-graph";
+import { fetchTests } from "@/redux/service";
+import { setTests } from "@/redux/features";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "@/assets/hoc/loading";
 
 const Dashboard = () => {
+  const recordtype = "Vase life analysis";
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const { tests } = useSelector((store) => store.test);
+
+  const getAllTests = async () => {
+    const res = await fetchTests(recordtype);
+    dispatch(setTests(res));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (tests.length === 0) {
+      getAllTests();
+    } else {
+      setLoading(false);
+    }
+  }, []);
   return (
     <section className="p-4 space-y-4">
-      <ActiveTestGraph />
-      <CompleteTestGraph />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <ActiveTestGraph />
+          <CompleteTestGraph />
+        </>
+      )}
     </section>
   );
 };
